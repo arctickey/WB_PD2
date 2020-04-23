eca3f8db870076800b7c8669c880096fabe32fca
jjanborowka <43787526+jjanborowka@users.noreply.github.com>
2020-04-23 17:40:54 +0200

    Q

--- VIM.R
+++ VIM.R
@@ -1,37 +1,23 @@
-VIM_knn <- function (data,path_to_jason){
-  
-  library(rjson)
-  
-  c <- fromJSON(file = path_to_jason)
+VIM_knn <- function (data){
   
   
-  w <- 1:length(colnames(data))
-  w <- w[ifelse(colnames(data)==c[5],TRUE,FALSE)]
   
-  w <- 1:length(colnames(data))
-  w <- w[ifelse(colnames(data)==c[5],TRUE,FALSE)]
+  
   #Imputacja danych dla zbioru treningowego i testoweg 
-  data[,-w] <- kNN(data[,-w])
+  data <- kNN(data)
   
   return(data)
 }
 
 
-VIM_irmi <-  function (data,path_to_jason){
-  
-  library(rjson)
-  
-  c <- fromJSON(file = path_to_jason)
+VIM_irmi <-  function (data){
   
   
-  w <- 1:length(colnames(data))
-  w <- w[ifelse(colnames(data)==c[5],TRUE,FALSE)]
   
-  w <- 1:length(colnames(data))
-  w <- w[ifelse(colnames(data)==c[5],TRUE,FALSE)]
-  dop <- length(colnames(data)) -1 
+ 
+  dop <- length(colnames(data)) 
   #Imputacja danych dla zbioru treningowego i testoweg 
-  data[,-w] <- irmi(data[,-w])[1:dop]
+  data<- irmi(data)[1:dop]
   
   return(data)
 }
