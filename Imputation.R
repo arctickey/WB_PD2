2d3fea51696d4b1febbabca52d2f83b67fff4ca5
arctickey <filipchrzuszcz1@gmail.com>
2020-04-27 12:31:03 +0200

    Update

--- Imputation.R
+++ Imputation.R
@@ -30,57 +30,68 @@ all_imputation <- function(data,target){
  
   ## soft wszystkie kolumny gdzie nie można uzyć pakietu chcę wypełniać modą 
   # zamknąłem w time_it dla bezpieczeństwa
+  time_softImpute <- Sys.time()
   data_softImpute <- data
   im_train <- impute_softimpute(data_softImpute[train_set,])
   im_test <- impute_softimpute(data_softImpute[test_set,])
   r <- rbind(im_train, im_test)
   ord <- as.numeric(row.names(r))
   data_softImpute <- r[order(ord), ]
+  time_softImpute <- Sys.time() - time_softImpute
   print('softImpute successful')
+  
 
   # missForest z tego co patrzyłem ogarniał wszyskie rodzaje zmiennych ale długo działa 
+  time_forest <- Sys.time()
   data_missForest <- data
   try(
   data_missForest[train_set,] <- misinf_forest_aut(data_missForest[train_set,]),
-  data_missForest[test_set,] <- misinf_forest_aut(data_missForest[test_set,]),
-  print('missForest successful'))
+  data_missForest[test_set,] <- misinf_forest_aut(data_missForest[test_set,]))
+  time_forest <- Sys.time() - time_forest
   
-
   
   
   # Uzupełnanie medianą/modą 
+  time_median <- Sys.time()
   data_median <- data
   data_median[train_set,] <- prepareMedian(data_median[train_set,])
   data_median[test_set,] <- prepareMedian(data_median[test_set,])
+  time_median <- Sys.time() - time_median
   print('modeMedian successful')
   
   #Mice
+  time_mice <- Sys.time()
   data_mice <- data
   data_mice[train_set,] <- prepareMice(data_mice[train_set,])
   data_mice[test_set,] <- prepareMice(data_mice[test_set,])
+  time_mice <- Sys.time() - time_mice
   print('Mice successful')
    
-
+  time_irmi <- Sys.time()
   data_irmi <- data
   data_irmi[train_set,] <- VIM_irmi(data_irmi[train_set,])
   data_irmi[test_set,] <- VIM_irmi(data_irmi[test_set,])
+  time_irmi <- Sys.time() - time_irmi
   print('VIM_irmi successful')
 
 
   
-  # missMDA 
+  #missMDA 
+  time_mda <- Sys.time()
   data_missMDA <- data
   data_missMDA[train_set,] <- impute_missMDA(data_missMDA[train_set,])
   data_missMDA[test_set,] <- impute_missMDA(data_missMDA[test_set,])
-  print('missmda success')
+  time_mda <- Sys.time() - time_mda
+
    
 
-  type_of_imputation <- list('median','irmi','mice','forest','mda')
-  datasets <- list(data_median,data_irmi,data_mice,data_missForest,data_missMDA)
+  type_of_imputation <- list('median','irmi','mice','forest','MDA','softImpute')
+  datasets <- list(data_median,data_irmi,data_mice,data_missForest,data_missMDA,data_softImpute)
+  times <- list(time_median,time_irmi,time_mice,time_forest,time_mda,time_softImpute)
 
 
   
-  return(list(type_of_imputation,datasets,train_set,test_set))
+  return(list(type_of_imputation,datasets,train_set,test_set,times))
 
   
 }
--- auto_pioter.R
+++ auto_pioter.R
@@ -3,38 +3,35 @@ library(mlr3)
 library(tidyverse)
 library(R.utils) 
 #setwd('./WB_PD2')
-#Wymaga podmiany reszta powinna działać
-path_to_datasets <- "/home/piotr/Programowanie/WB/fork_grupy/2020L-WarsztatyBadawcze-Imputacja/datasets"
-#path_to_datasets <- "/home/arctickey/2020L-WarsztatyBadawcze-Imputacja/datasets"
 
-scores = tibble(Dataset = numeric(),Method = character(),Score = numeric())
+#path_to_datasets <- "/home/piotr/Programowanie/WB/fork_grupy/2020L-WarsztatyBadawcze-Imputacja/datasets"
+path_to_datasets <- "/home/arctickey/2020L-WarsztatyBadawcze-Imputacja/datasets"
+
+scores = tibble(Dataset = numeric(),Method = character(),Acc = numeric(),Auc=numeric(),
+                recall = numeric(),precision=numeric())
 folder <- list.dirs(path_to_datasets)
 folder <- folder[-1]
 script_paths <- paste(folder, '/', 'code.R', sep='')
-
+out <- vector()
 #Przeniosłem bo jak widzę nie czyścisz env po każdym zbiorze to może być raz wczytane
 source("./Imputation.R")
 loading()
 
-pass <- c(1, 6, 7, 8, 10, 11)
 
-for(i in script_paths[-pass]){
+for(i in script_paths){
   source(i, chdir = TRUE)
-  
-  #Nie działa na zbiorach  29 - błędy w kodowaniu,
-  # nie działa tez na 40536 i 41278 -  za duze
-  
   #Poprawki na zbiorach
   if(openml_id == 6332){
     dataset <- subset(dataset, select = -timestamp)
   }
   
   imput_result <- all_imputation(dataset, target_column)
-  
   measure = msr("classif.acc")
+  auc = msr('classif.auc')
+  recall = msr('classif.recall')
+  precision = msr('classif.precision')
   k = length(imput_result[[1]])
   j = 1
-  
   try(
   while (j<=k){
     task= TaskClassif$new(id =imput_result[[1]][[j]] , backend =imput_result[[2]][[j]], target = target_column)
@@ -42,12 +39,16 @@ for(i in script_paths[-pass]){
     learner$train(task,row_ids = imput_result[[3]])
     prediction <- learner$predict(task,row_ids=imput_result[[4]])
     acc <- prediction$score(measure)
-    print(imput_result[[1]][[j]])
-    scores <- rbind(scores,c(openml_id,imput_result[[1]][[j]],acc))
+    au <- prediction$score(auc)
+    rec <- prediction$score(recall)
+    precis <- prediction$score(precision)
+    scores <- rbind(scores,c(openml_id,imput_result[[1]][[j]],acc,au,rec,precis,imput_result[[5]][[j]]))
     j= j+1
   }
   )
 }
 
-colnames(scores) <- c('Dataset','Method','Score')
-write.csv(scores, './wyniki_pioter.csv')
+colnames(scores) <- c('Dataset','Method','Acc','Auc','Recall','Precision','Time')
+scores
+write.csv(scores, './wyniki_moje.csv')
+
